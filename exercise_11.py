081196ef3b42c9b35d2c31f98ba92c35428a0de4
Fareeduddin Shaik <shaik882@gmail.com>
2020-06-03 21:49:08 +0530

    exercise_11 & exercise_12 - two new exercises

---
+++ exercise_11.py
@@ -0,0 +1,25 @@
+# Use trunc(), floor() and ceil() for numbers -2.8, -0.5, 0.2, 1.5 and 2.9 to understand the difference
+# between these functions clearly.
+import math
+
+x = -2.8
+y = -0.5
+z = 0.2
+a = 1.5
+b = 2.9
+print(math.ceil(x))
+print(math.ceil(y))
+# print(math.ceil(z))
+# print(math.ceil(a))
+# print(math.ceil(b))
+print(math.floor(x))
+print(math.floor(y))
+# print(math.floor(z))
+# print(math.floor(a))
+# print(math.floor(b))
+print(math.trunc(x))
+print(math.trunc(y))
+# print(math.trunc(y))
+# print(math.trunc(z))
+# print(math.trunc(a))
+# print(math.trunc(b))
